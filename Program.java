27b1451c9a9b4a3d7f167aeb6247ccfab25c23c4
Abhishek Borse <abhishekborse5015@gmail.com>
2022-09-13 13:10:44 +0530

    modified Program.java

--- Program.java
+++ Program.java
@@ -2,6 +2,6 @@ public class Program{
     public static void main(String args[]){
         int a = 8,b  = 7;
         int sum = a+b;
-        System.out.println(sum);
+        System.out.println('SUM IS ', sum);
     }
 }
