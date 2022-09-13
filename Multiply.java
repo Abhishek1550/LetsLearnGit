408eb00c479fec1f2673ebdb63f11b5301b1a3f0
Abhishek Borse <abhishekborse5015@gmail.com>
2022-09-13 13:48:17 +0530

    added Multiply.java into abhishek/multiply branch

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply{
+    public static void main(String args[]){
+        int a = 8,b  = 7;
+        int res = a*b;
+        System.out.println('Multiplication is ', res);
+    }
+}
