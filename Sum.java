86403b748346f5516f202d4905eab480407d5eec
Abhishek Borse <abhishekborse5015@gmail.com>
2022-09-14 07:32:41 +0530

    created diff program

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String args[]){
         int a = 8,b  = 7;
         int sum = a+b;
-        System.out.println('SUM IS ', sum);
+        System.out.println('Sun is ', sum);
     }
 }
